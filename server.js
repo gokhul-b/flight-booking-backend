ca1d5ce12130944adc0f91ff7659f9ac49c3bd34
gokhul-b <gokhulb2001@gmail.com>
2023-06-09 02:07:44 +0530

    corserror

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(function (req, res, next) {
 app.use(cors());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   // Add other necessary CORS headers here
   next();
 });
